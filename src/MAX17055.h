a7ef6c13d06ad2bc97fdcc7158e44a703c408fcd
Mehmet Günce Akkoyun <akkoyun@me.com>
2024-02-20 13:42:02 +0300

    Bug fix

--- src/MAX17055.h
+++ src/MAX17055.h
@@ -426,7 +426,7 @@
 				I2C_Functions::Begin();
 
 				// Control for Device
-				if (I2C_Functions::Variables.Device.Detect) {
+				if (I2C_Functions::Detect()) {
 
 					// Set Configuration
 					this->Config();
